6a930c7c91851bef5b1cba1e35e1642ac424aabf
Cristian Palomeque <cristianpalocseres@gmail.com>
2022-04-12 15:45:10 -0500

    fixing posts service

--- querysSql/postsQuerys.js
+++ querysSql/postsQuerys.js
@@ -14,17 +14,17 @@ export const intoQueryCreatePost = (
 
 //get posts
 export const selectQueryGetposts = () => {
-  return `SELECT  P.id, P.userid, U.username, U.avatar, P.description, P.image, P.created_at, P.category, P.likes FROM posts as P INNER JOIN users as U WHERE P.userid=U.id`
+  return `SELECT  P.id, P.userid, U.username, U.avatar, P.description, P.image, P.video, P.created_at, P.category, P.likes FROM posts as P INNER JOIN users as U WHERE P.userid=U.id`
 }
 
 //get post to id 
 export const selectQueryGetPostToId = (id) => {
-  return `SELECT  P.id, P.userid, U.username, U.avatar, P.description, P.image, P.created_at, P.category, P.likes FROM posts as P INNER JOIN users as U WHERE  P.id = '${id}' && P.userid=U.id`
+  return `SELECT  P.id, P.userid, U.username, U.avatar, P.description, P.image, P.video, P.created_at, P.category, P.likes FROM posts as P INNER JOIN users as U WHERE  P.id = '${id}' && P.userid=U.id`
 }
 
 //get all posts to user
 export const selectQueryGetAllPostToUserId = (userid) => {
-  return `SELECT  P.id, P.userid, U.username, U.avatar, P.description, P.image, P.created_at, P.category, P.likes FROM posts as P INNER JOIN users as U WHERE  P.userid = '${userid}' && P.userid=U.id`
+  return `SELECT  P.id, P.userid, U.username, U.avatar, P.description, P.image, P.video, P.created_at, P.category, P.likes FROM posts as P INNER JOIN users as U WHERE  P.userid = '${userid}' && P.userid=U.id`
 }
 
 //edite post
